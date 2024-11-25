23007449c30e186f50360f4c87325ed388cb45c9
Jancarlos042003 <U21302940@utp.edu.pe>
2024-11-25 18:32:40 -0500

    Permitiendo dando permisos para mostrar el inventario

--- src/main/java/com/example/backend/security/SecurityConfig.java
+++ src/main/java/com/example/backend/security/SecurityConfig.java
@@ -66,6 +66,7 @@ public class SecurityConfig {
                     auth.requestMatchers(HttpMethod.DELETE, "/api/autor/**").hasRole("ADMIN");
 
                     auth.requestMatchers("/api/inventario/**").hasRole("ADMIN");
+                    auth.requestMatchers(HttpMethod.GET, "/api/inventario").hasRole("USER");
 
                     auth.requestMatchers(HttpMethod.GET, "/api/editorial").permitAll();
                     auth.requestMatchers(HttpMethod.POST, "/api/editorial").hasRole("ADMIN");
