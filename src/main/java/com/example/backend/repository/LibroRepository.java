d02a11bb05108df40e4f6e8df3a85b7f6c57c44a
Jancarlos042003 <U21302940@utp.edu.pe>
2024-11-29 23:52:53 -0500

    Se agrego un query personalizado para poder filtrar los libros y solo obtener sus campos necesarios

--- src/main/java/com/example/backend/repository/LibroRepository.java
+++ src/main/java/com/example/backend/repository/LibroRepository.java
@@ -1,5 +1,6 @@
 package com.example.backend.repository;
 
+import com.example.backend.dto.LibroCardDTO;
 import com.example.backend.model.Libro;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -17,4 +18,11 @@ public interface LibroRepository extends JpaRepository<Libro, Long> {
             "LOWER(l.autor) LIKE LOWER(CONCAT('%', :termino, '%'))")
     List<Libro> buscarPorCriterios(@Param("termino") String termino);
     Optional<List<Libro>> findByCategoriasId(Long id);
+
+    @Query("SELECT new com.example.backend.dto.LibroCardDTO(l.id, l.titulo, l.autor, l.precio, l.descuento, l.descripcion, l.imgPortada) " +
+            "FROM Libro l " +
+            "JOIN l.categorias c " +
+            "WHERE c.id = :categoriaId")
+    List<LibroCardDTO> findLibroCardsByCategoriaId(@Param("categoriaId") Long categoriaId);
+
 }
