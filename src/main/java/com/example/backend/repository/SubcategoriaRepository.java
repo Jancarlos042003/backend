837ec8f31904e6997f1ada24a1834b19235e2484
Jancarlos042003 <U21302940@utp.edu.pe>
2025-02-20 17:37:36 -0500

    Se eleminó todo lo referente a subcategorias. Se agregaron diferentes ambientes(dev, prod)

--- src/main/java/com/example/backend/repository/SubcategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.model.Subcategoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SubcategoriaRepository extends JpaRepository<Subcategoria,Long> {
-}
